efb1bfa49afa35fd09c6a6e6263e749174f929d4
Teffen Ellis <teffen@goauthentik.io>
2025-08-03 18:36:41 +0200

    web: Remove extra-wide field groups.

--- web/src/admin/providers/oauth2/OAuth2ProviderFormForm.ts
+++ web/src/admin/providers/oauth2/OAuth2ProviderFormForm.ts
@@ -187,7 +187,6 @@ export function renderForm(
                 >
                 </ak-hidden-text-input>
                 <ak-form-element-horizontal
-                    flow-direction="row"
                     label=${msg("Redirect URIs/Origins (RegEx)")}
                     name="redirectUris"
                 >
